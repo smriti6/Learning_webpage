b54ad2e46b671b86951364ff6fbeae71b9fcbcd4
smriti6 <smritis481@gmail.com>
2022-08-09 14:52:51 +0530

    project

---
+++ HTML Project/second/source/index.js
@@ -0,0 +1,53 @@
+// document.addEventListerner('DOMContentLoaded', () => {
+
+// })
+
+
+//variable 
+var x=2;
+x=8;
+var x='s';
+
+//output
+console.log(x);
+console.log('Hello console');
+
+
+//function
+function printName() {
+    console.log('Sam');
+}
+
+printName();
+
+const z='const';
+//cant be updated
+// z='h';
+console.log(z);
+
+function find() {
+    var y=4;
+    {
+        let y=6;
+        console.log('Inside loop : '+y);
+    }
+    //without let print 6
+    console.log('Outside : '+y);
+}
+
+find();
+
+//exercise 
+console.log('exercise');
+let count=1;
+function increment() {
+    return count+=1;
+}
+console.log(increment());
+
+const name='cake';
+function sendText() {
+    console.log('Hay '+name+ ' you are sweet')
+}
+
+sendText();
