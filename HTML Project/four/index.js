a723dc56fc76cdde52be65e09fcb2d5e6b7a2b85
smriti6 <smritis481@gmail.com>
2022-09-04 18:38:18 +0530

    project

---
+++ HTML Project/four/index.js
@@ -0,0 +1,14 @@
+const cir=document.querySelectorAll('.circle');
+console.log(cir.length);
+
+
+//concat();
+const name='sam is ,';
+const str2=' cool';
+console.log(name.concat(str2));
+console.log(name);
+
+const ar1=[1,2,3,4];
+const ar2=[5,6,7,8];
+console.log(ar1.concat(ar2));
+console.log(ar1);
