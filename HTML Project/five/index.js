6456667a3b5c34c799c60d02ced7421ce1c2f1e2
smriti6 <smritis481@gmail.com>
2022-09-05 23:03:55 +0530

    project

---
+++ HTML Project/five/index.js
@@ -0,0 +1,10 @@
+const textDesplay=document.querySelector('.text');
+
+
+//join
+
+const ar1=['cat','dog','rat'];
+const joi=ar1.join(' ');
+textDesplay.innerHTML='test';
+textDesplay.innerHTML=ar1;
+textDesplay.innerHTML=joi;
